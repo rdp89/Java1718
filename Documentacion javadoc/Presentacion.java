37619372f1f691cd7e01839a044a515a84bbe470
Rafa Delgado <rafadelgado89@gmail.com>
2017-09-27 12:20:47 +0200

    Ejercicio Hola Mundo y Documentación Javadoc

---
+++ Documentacion javadoc/Presentacion.java
@@ -0,0 +1,35 @@
+/**
+*
+* <p>Javadoc es una herramienta para generar documentación de la API en formato HTML. Se genera a partir de comentarios de documentos escritos en el código fuente.
+* La documentación javadoc abarca paquetes, clases, interfaces, campos y métodos.</p>
+*
+*<p>Crea una clase "Presentacion" que muestre en tres líneas distintas:</p>
+*<ul>
+*  <li>tu nombre y apellidos</li>
+*  <li>tu edad </li>
+*  <li>lugar de nacimiento</li>
+*</ul>
+*
+*<p>Documenta la clase Presentacion. La documentación ha de contener:</p>
+*<ul>
+*  <li>este enunciado</li>
+*  <li>nombre del autor</li>
+*  <li>versión de la aplicación.</li>
+*</ul>
+*
+*<p>Entrega un fichero comprimido que contenga el fichero fuente "Presentacion.java" y toda su documentación generada mediante javadoc.</p>
+*@author Rafael Delgado Peña
+*@version 1.0
+
+*/
+public class Presentacion{
+/**
+Aquí empieza el método main
+@param args que contiene la cadena de caracteres del método main
+*/
+	public static void main(String[] args){
+		System.out.println("Rafael Delgado Peña");
+		System.out.println("28 años");
+		System.out.println("Córdoba");
+	}
+}
---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,6 @@
+class HolaMundo {
+public static void main(String[] args){
+	System.out.println("Hola Mundo!!");
+	System.out.println("Soy Rafael Delgado Pe\u00f1a");
+	}
+}
